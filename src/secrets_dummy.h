9f428ff113e338573ceaf3a6f02bc3f81a0d6062
T. Vandenryt <thijs.vandenryt@uhasselt.be>
2024-07-23 10:50:42 +0200

    Test_Update

---
+++ src/secrets_dummy.h
@@ -0,0 +1,8 @@
+//WiFi settings
+const char* SSID = "SSIDName";
+const char* PASSWORD = "WiFiPassword";
+
+//MQTT settings
+#define Broker "broker.hivemq.com"
+#define Port 1883
+#define Topic "Topicname"
