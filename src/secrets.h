6a963a98f8cf744029ab11a24f380e69cc2e80ff
T. Vandenryt <thijs.vandenryt@uhasselt.be>
2024-07-23 10:56:03 +0200

    BlaBla

--- src/secrets.h
+++
@@ -1,8 +0,0 @@
-//WiFi settings
-const char* SSID = "GastWifi";
-const char* PASSWORD = "GastInternet2";
-
-//MQTT settings
-#define Broker "broker.hivemq.com"
-#define Port 1883
-#define Topic "Thijs/C3"
